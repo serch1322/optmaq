469d65410d4fa17dd7f9f8bdb33ea060da5dc24e
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-07-09 00:40:48 -0500

    QR en Mantenimiento

---
+++ qr_mantenimiento/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'QR Mantenimiento',
+    'summary': 'Creación de QR por Equipo de Mantenimiento',
+    "version": "14.0.1.0.0",
+    "author": "IT Reingenierias",
+    'category': 'Industries',
+    "website": "https://www.itreingenierias.com/",
+    'depends': [
+        'maintenance',
+        'base',
+    ],
+    'data': [
+        'views/mantenimiento.xml',
+    ],
+    'installable': True,
+}
---
+++ qr_mantenimiento/models/mantenimiento.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import qrcode
+import base64
+
+from io import BytesIO
+
+from odoo import models, fields, _
+from odoo.exceptions import UserError
+
+class QRMantenimiento(models.Model):
+    _inherit = ['maintenance.equipment']
+
+    codigo_qr = fields.Binary('Codigo QR')
+
+    def generar_qr(self):
+        if self.id:
+            data = self.id
+            base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.make("%s/web#id=%s&action=344&model=c&view_type=form&cids=1&menu_id=227"%(base_url,data),fit=True)
+            img = qr.make_image()
+            tmp = BytesIO()
+            img.save(tmp, format="PNG")
+            qr_img = base64.b64encode(tmp.getvalue())
+            self.codigo_qr = qr_img
+        else:
+            None
