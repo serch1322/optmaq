6f2c7110bef512de6bcad27fa5c784bd15f1300d
serch1322 <sergio.rodriguez@itreingenierias.com>
2021-07-13 11:33:09 -0500

    Crear MTO Correctivo

--- mantenimiento_preventivo/models/mantenimiento_equipo.py
+++ mantenimiento_preventivo/models/mantenimiento_equipo.py
@@ -1,12 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from datetime import datetime, date, timedelta
 
 
 class MantenimientoPreventivo(models.Model):
     _inherit = 'maintenance.equipment'
 
     tipo_mantenimiento = fields.One2many('mantenimiento.preventivo','equipo',string="Tipo de Mantenimiento", ondelete='cascade')
+    
+    def mantenimiento_correctivo(self):
+        correctivo = self.env['maintenance.request']
+        valores_equipo = {}
+        valores_equipo.update({
+            'name': "Mantenimiento Correctivo",
+            'equipment_id': self.id,
+            'request_date': date.today(),
+            'type': 'corrective',
+            'maintenance_team_id': self.maintenance_team_id.id,
+            'user_id': self.technician_user_id.id,
+            'scheduled_date': date.today(),
+        })
+        mante_creado = correctivo.create(valores_equipo)
+        return{
+             'type': 'ir.actions.act_window',
+             'res_model': 'maintenance.request',
+             'view_type': 'form',
+             'view_mode': 'tree,form',
+             'target': 'self',
+        }
+
+
 
 class TipodePreventivo(models.Model):
     _name = 'mantenimiento.preventivo'
